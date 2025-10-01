13f18a9759b3d76fecc860c6b4b445cda793bb3d
Julien Pillis <julien.pillis@etu.utc.fr>
2025-10-01 14:15:22 +0200

    Fix resize slider

--- Assets/Resources/General/HorizontalScaler.cs
+++ Assets/Resources/General/HorizontalScaler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6b6c305f45c4d8784f2409831d58e1afce85604b2b37c57ab6efcd9652c8bbd
-size 1324
+oid sha256:bc990eee376ea4c33020b977c89bafb0c918e40ab2315ab67f3100798ec75b6a
+size 1718
