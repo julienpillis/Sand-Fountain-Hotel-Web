dddb0e242d0eec21d74048328b7b0e5bc85a1c23
Julien Pillis <julien.pillis@etu.utc.fr>
2025-10-09 10:33:55 +0200

    Added intro

--- Assets/Resources/General/ScenarU/Triggers/StartTrigger.cs
+++ Assets/Resources/General/ScenarU/Triggers/StartTrigger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e286884e887f99ff862b0bfc13f0ac17f5b72149d47ff9786303900943f2ddbb
-size 160
+oid sha256:ce7526905f32c95f5de8f7afca5190a25f75e7e551f552afff7ec2d5c865d1c0
+size 545
--- Assets/Scenes/Galactic Center/parking/LoadSceneAction.cs
+++ Assets/Scenes/Galactic Center/parking/LoadSceneAction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c67577f5d85fecd0140837f37f88fffad7632d5e894b767b05792c2ae0dd4df
-size 1819
+oid sha256:5181889ea2bdad53a20beb51960f74a99c432366bcfb25ec71bbe6defee33e38
+size 1682
